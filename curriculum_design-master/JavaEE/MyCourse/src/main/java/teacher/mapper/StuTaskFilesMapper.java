438fbf339ee43b3a4c8dfcecd6c2c5fd474b6a0d
mkslaj <42075815+mkslaj@users.noreply.github.com>
2023-02-10 02:33:30 +0800

    通讯录

--- curriculum_design-master/JavaEE/MyCourse/src/main/java/teacher/mapper/StuTaskFilesMapper.java
+++
@@ -1,5 +0,0 @@
-package teacher.mapper;
-
-public interface StuTaskFilesMapper {
-
-}
--- curriculum_design-master/JavaEE/MyCourse/src/main/java/teacher/service/TeaCourseStudentService.java
+++
@@ -1,9 +0,0 @@
-package teacher.service;
-
-import login.pojo.User;
-
-import java.util.List;
-
-public interface TeaCourseStudentService {
-    List<User> findAllByCourseId(Integer courseId);
-}
